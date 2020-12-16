fe50e5acf81a600bec4f9e8d1e440e91ec921547
mattmck <matthew.mckerroll@gmail.com>
2020-12-15 23:46:26 -0500

    added unique ID module, made some server.js changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('./routes/api_routes');
 require('./routes/html_routes');
+const notesdb = require('./db/db.json');
 
 const app = express();
 
@@ -10,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
+require("./routes/api_routes")(app);
+require("./routes/html_routes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
